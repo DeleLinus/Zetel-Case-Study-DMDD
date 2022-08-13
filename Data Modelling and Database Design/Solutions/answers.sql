677ebbc4e6fa2f6effceb6b1e3328a4de81dacb2
Ayanwoye  Gideon Ayandele <58152694+DeleLinus@users.noreply.github.com>
2022-08-13 15:29:25 +0100

    Add files via upload

---
+++ Data Modelling and Database Design/Solutions/answers.sql
@@ -0,0 +1,277 @@
+-- 1---------------------------------
+/*SELECT 
+    COUNT(DISTINCT (ConnectID)) AS connect_count
+FROM
+    mobile.connect;*/
+
+
+-- 2---------------------------------
+/*SELECT 
+    *
+FROM
+    staff
+WHERE
+    LCASE(Address) LIKE '%st%';*/
+
+
+-- 3---------------------------------
+/*SELECT DISTINCT
+    (c.CustomerID),
+    CONCAT(c.Surname, ' ', c.Given) AS InctiveCustomersName
+FROM
+    customer c
+        JOIN
+    mobile m ON (c.CustomerID = m.CustomerID)
+WHERE
+    m.Cancelled IS NOT NULL;*/
+
+
+-- 4---------------------------------
+/*SELECT 
+    PlanName
+FROM
+    plan
+ORDER BY DataAllowance DESC
+LIMIT 1 , 1;*/
+
+
+-- 5---------------------------------
+/*SELECT 
+    StaffID, Surname, Given, DOB, Sex, Suburb, RatePerHour
+FROM
+    mobile.staff
+WHERE
+    RatePerHour > (SELECT 
+            AVG(RatePerHour)
+        FROM
+            mobile.staff);*/
+
+
+-- 6---------------------------------
+/*SELECT 
+    PlanName, COUNT(PlanName) AS count
+FROM
+    mobile.mobile
+GROUP BY PlanName
+ORDER BY count DESC
+LIMIT 2;*/
+
+
+-- 7---------------------------------
+/*SELECT 
+    CustomerID, Surname, Given, Sex, Suburb
+FROM
+    customer
+WHERE
+    CustomerID IN (SELECT 
+            CustomerID
+        FROM
+            mobile
+        WHERE
+            PlanName = 'ExtraSmall');*/
+
+
+-- 8---------------------------------
+/*SELECT 
+    t2.TowerID,
+    DATEDIFF(CURRENT_TIMESTAMP(), t1.CallDate) AS recency
+FROM
+    calls t1,
+    connect t2
+WHERE
+    t1.CallsID = t2.CallsID
+ORDER BY recency ASC;*/
+
+
+-- 9---------------------------------
+/*SELECT 
+    CustomerID, Surname
+FROM
+    customer
+WHERE
+    CustomerID IN (SELECT DISTINCT
+            (CustomerID)
+        FROM
+            mobile
+        WHERE
+            PhoneColour IN (SELECT 
+                    PhoneColour
+                FROM
+                    mobile
+                WHERE
+                    CustomerID IN (SELECT 
+                            CustomerID
+                        FROM
+                            customer
+                        WHERE
+                            Surname = 'Rajoo')));*/
+
+-- 10---------------------------------
+/*SELECT 
+    c.CustomerID, c.Surname, c.Given
+FROM
+    customer c,
+    (SELECT 
+        CustomerID, COUNT(CustomerID) AS number_of_phone
+    FROM
+        mobile
+    GROUP BY CustomerID
+    HAVING number_of_phone > 1) many_phone
+WHERE
+    c.CustomerID = many_phone.CustomerID;*/
+
+
+-- 11------------------------------------
+/*SET SQL_SAFE_UPDATES = 0;
+
+UPDATE mobile 
+SET 
+    PlanName = 'Budget3Extra'
+WHERE
+    PlanName = 'Budget3';
+
+UPDATE plan 
+SET 
+    PlanName = 'Budget3Extra'
+WHERE
+    PlanName = 'Budget3';
+
+SET SQL_SAFE_UPDATES = 1;*/
+
+
+-- 12---------------------------------
+/*SELECT 
+    staff_with_supervisor.StaffID,
+    staff_with_supervisor.Surname,
+    staff_with_supervisor.Given,
+    CONCAT(s.Surname, ', ', s.Given) AS Supervisor_Names
+FROM
+    staff s,
+    (SELECT 
+        StaffID, Surname, Given, SupervisorID
+    FROM
+        staff
+    WHERE
+        SupervisorID > 0) staff_with_supervisor
+WHERE
+    staff_with_supervisor.SupervisorID = s.StaffID;*/
+   
+   
+-- 13---------------------------------
+/*SELECT 
+    StaffID,
+    CONCAT(Surname, ', ', Given) AS FullName,
+    DATE(Joined) AS Joined,
+    TIMESTAMPDIFF(YEAR, Joined, CURDATE()) AS TotalYears
+FROM
+    staff
+ORDER BY TotalYears DESC
+LIMIT 1;*/
+
+
+-- 14a---------------------------------
+/*SELECT 
+    PlanName
+FROM
+    plan
+WHERE
+    PlanName NOT IN (SELECT DISTINCT
+            (PlanName)
+        FROM
+            mobile);*/
+            
+-- 14b---------------------------------
+/*SELECT DISTINCT
+    (p.PlanName)
+FROM
+    plan p
+        LEFT JOIN
+    mobile m ON (p.PlanName = m.PlanName)
+WHERE
+    m.PlanName IS NULL;*/
+    
+    
+-- 15---------------------------------
+/*SELECT 
+    cs.CustomerID,
+    CONCAT(cs.Surname, ', ', cs.Given) AS CustomerName,
+    m.PhoneNumber,
+    SUM(c.CallDuration) / 3600 AS TotalHours
+FROM
+    calls c
+        JOIN
+    mobile m ON c.MobileID = m.MobileID
+        JOIN
+    customer cs ON m.CustomerID = cs.CustomerID
+WHERE
+    (MONTHNAME(c.CallDate) = 'August'
+        AND YEAR(c.CallDate) = 2019)
+GROUP BY m.PhoneNumber
+ORDER BY TotalHours DESC;*/
+
+
+-- 16---------------------------------
+/*CREATE VIEW PopularityView AS
+    SELECT 
+        PlanName, COUNT(PlanName) AS populaity
+    FROM
+        mobile
+    GROUP BY PlanName;
+
+SELECT 
+    PlanName
+FROM
+    PopularityView
+ORDER BY populaity DESC
+LIMIT 1;*/
+
+
+-- 17---------------------------------
+/*SELECT 
+    t.Location, COUNT(t.Location) AS TotalConnections
+FROM
+    connect co
+        JOIN
+    calls ca ON (co.CallsID = ca.CallsID)
+        JOIN
+    tower t ON (co.TowerID = t.TowerID)
+WHERE
+    YEAR(CallDate) > 2018
+GROUP BY t.Location;*/
+
+
+-- 18---------------------------------
+/*(SELECT 
+    c.*,
+    m.BrandName,
+    TIMESTAMPDIFF(YEAR, m.Joined, CURDATE()) AS YearofUsage
+FROM
+    customer c
+        JOIN
+    mobile m ON (c.CustomerID = m.CustomerID)
+WHERE
+    (m.BrandName = 'Samsung')
+ORDER BY YearofUsage
+LIMIT 1) UNION ALL (SELECT 
+    c.*,
+    m.BrandName,
+    TIMESTAMPDIFF(YEAR, m.Joined, CURDATE()) AS YearofUsage
+FROM
+    customer c
+        JOIN
+    mobile m ON (c.CustomerID = m.CustomerID)
+WHERE
+    (m.BrandName = 'Samsung')
+ORDER BY YearofUsage DESC
+LIMIT 1);*/
+
+
+-- 19---------------------------------
+/*SELECT 
+    MONTHNAME(Joined) AS JoinedMonth,
+    Joined AS JoinedDate,
+    CONCAT(Surname, ' ', Given) AS StaffName
+FROM
+    staff
+ORDER BY JoinedMonth DESC;*/
+
---
+++ Data Modelling and Database Design/sql_scripts/mobile_plan.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mobile
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `plan`
+--
+
+DROP TABLE IF EXISTS `plan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `plan` (
+  `PlanName` varchar(20) NOT NULL,
+  `BreakFee` decimal(8,2) DEFAULT NULL,
+  `DataAllowance` int DEFAULT '0',
+  `MonthlyFee` decimal(8,2) DEFAULT NULL,
+  `PlanDuration` int NOT NULL,
+  `CallCharge` int NOT NULL,
+  PRIMARY KEY (`PlanName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `plan`
+--
+
+LOCK TABLES `plan` WRITE;
+/*!40000 ALTER TABLE `plan` DISABLE KEYS */;
+INSERT INTO `plan` VALUES ('Budget1',0.00,0,0.00,0,10),('Budget3',0.00,1,15.00,0,10),('ExtraSmall',100.00,5,25.00,36,0),('Large',400.00,60,50.00,24,0),('Medium',300.00,40,50.00,24,0),('Small',200.00,20,30.00,36,0);
+/*!40000 ALTER TABLE `plan` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-08  7:13:11
