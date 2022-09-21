74a187430fc1f5d64e11fa36effa45aa5b970e7a
Ayanwoye  Gideon Ayandele <58152694+DeleLinus@users.noreply.github.com>
2022-09-21 23:55:40 +0100

    Update answers_script.sql

--- answers_script.sql
+++ answers_script.sql
@@ -1,21 +1,21 @@
 -- 1---------------------------------
-/*SELECT 
+SELECT 
     COUNT(DISTINCT (ConnectID)) AS connect_count
 FROM
-    mobile.connect;*/
+    mobile.connect;
 
 
 -- 2---------------------------------
-/*SELECT 
+SELECT 
     *
 FROM
     staff
 WHERE
-    LCASE(Address) LIKE '%st%';*/
+    LCASE(Address) LIKE '%st%';
 
 
 -- 3---------------------------------
-/*SELECT DISTINCT
+SELECT DISTINCT
     (c.CustomerID),
     CONCAT(c.Surname, ' ', c.Given) AS InctiveCustomersName
 FROM
@@ -23,20 +23,20 @@ FROM
         JOIN
     mobile m ON (c.CustomerID = m.CustomerID)
 WHERE
-    m.Cancelled IS NOT NULL;*/
+    m.Cancelled IS NOT NULL;
 
 
 -- 4---------------------------------
-/*SELECT 
+SELECT 
     PlanName
 FROM
     plan
 ORDER BY DataAllowance DESC
-LIMIT 1 , 1;*/
+LIMIT 1 , 1;
 
 
 -- 5---------------------------------
-/*SELECT 
+SELECT 
     StaffID, Surname, Given, DOB, Sex, Suburb, RatePerHour
 FROM
     mobile.staff
@@ -44,21 +44,21 @@ WHERE
     RatePerHour > (SELECT 
             AVG(RatePerHour)
         FROM
-            mobile.staff);*/
+            mobile.staff);
 
 
 -- 6---------------------------------
-/*SELECT 
+SELECT 
     PlanName, COUNT(PlanName) AS count
 FROM
     mobile.mobile
 GROUP BY PlanName
 ORDER BY count DESC
-LIMIT 2;*/
+LIMIT 2;
 
 
 -- 7---------------------------------
-/*SELECT 
+SELECT 
     CustomerID, Surname, Given, Sex, Suburb
 FROM
     customer
@@ -68,11 +68,11 @@ WHERE
         FROM
             mobile
         WHERE
-            PlanName = 'ExtraSmall');*/
+            PlanName = 'ExtraSmall');
 
 
 -- 8---------------------------------
-/*SELECT 
+SELECT 
     t2.TowerID,
     DATEDIFF(CURRENT_TIMESTAMP(), t1.CallDate) AS recency
 FROM
@@ -80,11 +80,11 @@ FROM
     connect t2
 WHERE
     t1.CallsID = t2.CallsID
-ORDER BY recency ASC;*/
+ORDER BY recency ASC;
 
 
 -- 9---------------------------------
-/*SELECT 
+SELECT 
     CustomerID, Surname
 FROM
     customer
@@ -104,10 +104,10 @@ WHERE
                         FROM
                             customer
                         WHERE
-                            Surname = 'Rajoo')));*/
+                            Surname = 'Rajoo')));
 
 -- 10---------------------------------
-/*SELECT 
+SELECT 
     c.CustomerID, c.Surname, c.Given
 FROM
     customer c,
@@ -118,11 +118,11 @@ FROM
     GROUP BY CustomerID
     HAVING number_of_phone > 1) many_phone
 WHERE
-    c.CustomerID = many_phone.CustomerID;*/
+    c.CustomerID = many_phone.CustomerID;
 
 
 -- 11------------------------------------
-/*SET SQL_SAFE_UPDATES = 0;
+SET SQL_SAFE_UPDATES = 0;
 
 UPDATE mobile 
 SET 
@@ -136,11 +136,11 @@ SET
 WHERE
     PlanName = 'Budget3';
 
-SET SQL_SAFE_UPDATES = 1;*/
+SET SQL_SAFE_UPDATES = 1;
 
 
 -- 12---------------------------------
-/*SELECT 
+SELECT 
     staff_with_supervisor.StaffID,
     staff_with_supervisor.Surname,
     staff_with_supervisor.Given,
@@ -154,11 +154,11 @@ FROM
     WHERE
         SupervisorID > 0) staff_with_supervisor
 WHERE
-    staff_with_supervisor.SupervisorID = s.StaffID;*/
+    staff_with_supervisor.SupervisorID = s.StaffID;
    
    
 -- 13---------------------------------
-/*SELECT 
+SELECT 
     StaffID,
     CONCAT(Surname, ', ', Given) AS FullName,
     DATE(Joined) AS Joined,
@@ -166,11 +166,11 @@ WHERE
 FROM
     staff
 ORDER BY TotalYears DESC
-LIMIT 1;*/
+LIMIT 1;
 
 
 -- 14a---------------------------------
-/*SELECT 
+SELECT 
     PlanName
 FROM
     plan
@@ -178,21 +178,21 @@ WHERE
     PlanName NOT IN (SELECT DISTINCT
             (PlanName)
         FROM
-            mobile);*/
+            mobile);
             
 -- 14b---------------------------------
-/*SELECT DISTINCT
+SELECT DISTINCT
     (p.PlanName)
 FROM
     plan p
         LEFT JOIN
     mobile m ON (p.PlanName = m.PlanName)
 WHERE
-    m.PlanName IS NULL;*/
+    m.PlanName IS NULL;
     
     
 -- 15---------------------------------
-/*SELECT 
+SELECT 
     cs.CustomerID,
     CONCAT(cs.Surname, ', ', cs.Given) AS CustomerName,
     m.PhoneNumber,
@@ -207,11 +207,11 @@ WHERE
     (MONTHNAME(c.CallDate) = 'August'
         AND YEAR(c.CallDate) = 2019)
 GROUP BY m.PhoneNumber
-ORDER BY TotalHours DESC;*/
+ORDER BY TotalHours DESC;
 
 
 -- 16---------------------------------
-/*CREATE VIEW PopularityView AS
+CREATE VIEW PopularityView AS
     SELECT 
         PlanName, COUNT(PlanName) AS populaity
     FROM
@@ -223,11 +223,11 @@ SELECT
 FROM
     PopularityView
 ORDER BY populaity DESC
-LIMIT 1;*/
+LIMIT 1;
 
 
 -- 17---------------------------------
-/*SELECT 
+SELECT 
     t.Location, COUNT(t.Location) AS TotalConnections
 FROM
     connect co
@@ -237,11 +237,11 @@ FROM
     tower t ON (co.TowerID = t.TowerID)
 WHERE
     YEAR(CallDate) > 2018
-GROUP BY t.Location;*/
+GROUP BY t.Location;
 
 
 -- 18---------------------------------
-/*(SELECT 
+(SELECT 
     c.*,
     m.BrandName,
     TIMESTAMPDIFF(YEAR, m.Joined, CURDATE()) AS YearofUsage
@@ -263,15 +263,15 @@ FROM
 WHERE
     (m.BrandName = 'Samsung')
 ORDER BY YearofUsage DESC
-LIMIT 1);*/
+LIMIT 1);
 
 
 -- 19---------------------------------
-/*SELECT 
+SELECT 
     MONTHNAME(Joined) AS JoinedMonth,
     Joined AS JoinedDate,
     CONCAT(Surname, ' ', Given) AS StaffName
 FROM
     staff
-ORDER BY JoinedMonth DESC;*/
+ORDER BY JoinedMonth DESC;
 
